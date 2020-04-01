6cbd625f157a330741a1f95ce0abb538896c0dcc
Leonardo <lgspezia@yahoo.com.br>
2020-04-01 07:33:36 -0300

    melhorias
    melhorias no cod

---
+++ js/formdata.js
@@ -0,0 +1,34 @@
+  
+  function httpGetAsync(theUrl, callback)
+  {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() { 
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+              callback(xmlHttp.responseText);
+      }
+      xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+      xmlHttp.send(null);
+  }
+  
+  function post(path, params, method='post') {
+  
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+  
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+  
+      form.appendChild(hiddenField);
+    }
+  }
+  
+  document.body.appendChild(form);
+  form.submit();
+  }
---
+++ src/main/resources/db/migration/V02__criar_insercoes_de_vinhos.sql
@@ -0,0 +1,8 @@
+INSERT INTO produto (nome, tipo_produto, classificacao_produto, volume, teor_alcoolico)
+values ('Partridge Reserva Pinot Noir', 'TINTO', 'SECO', 750, 13.50);
+
+INSERT INTO produto (nome, tipo_produto, classificacao_produto, volume, teor_alcoolico)
+values ('Tenuta Sant`Antonio Scaia Igt Bianco', 'BRANCO', 'DEMI_SEC', 750, 12.50);
+
+INSERT INTO produto (nome, tipo_produto, classificacao_produto, volume, teor_alcoolico)
+values ('Dominio Del Plata Malbec Rosé', 'ROSE', 'SECO', 750, 12.50);
